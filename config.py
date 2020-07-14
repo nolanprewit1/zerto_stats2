cf53c1506025420254515d21b8d50867d66d9f99
Parker Prewit <nolanprewit1@gmail.com>
2020-07-14 12:49:09 -0500

    removed uncessary files

--- config.py
+++
@@ -1,7 +0,0 @@
-class Config():
-    database_file = "../zerto_stats2.db"
-    zerto_analytics_username = "pprewit@involta.com"
-    zerto_analytics_password = "2mu#V%B6GeCH"
-    zerto_analytics_url = "https=//analytics.api.zerto.com/v2/"
-    poll_interval_minutes = 1
-    purge_records_days = 30
