e9771c32a4b075a05e0a3fb0c199de2e6f910f57
Parker Prewit <nolanprewit1@gmail.com>
2020-07-14 21:55:23 -0500

    updated init to reflect gui

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,5 +1,6 @@
 ### IMPORT REQUIRED PYTHON MODULES ###
-import json
+import json, os
+from dotenv import load_dotenv
 from flask import Flask, redirect, url_for
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,31 +8,31 @@ from sqlalchemy.orm import relationship, backref, sessionmaker, joinedload
 from sqlalchemy.pool import StaticPool
 
 ### IMPORT CONFIG FILE ### 
-with open("config.json") as config_file:
-    config = json.load(config_file)
+load_dotenv(os.path.join( os.getcwd(), '..', '.env' ))
 
 ### CONNECT TO THE DATABASE ###
 try:
-    db_path = "sqlite:///" + config.get("database_file")
+    db_path = "sqlite:///" + os.getenv("database_file")
     db_engine = create_engine(
         db_path, 
         echo=False, 
-        connect_args={'check_same_thread':False},
+        connect_args={"check_same_thread":False},
         poolclass=StaticPool)
     Session = sessionmaker(bind=db_engine)
     db_connection = Session()
 except:
     print("There was an error connecting to the database...")
 
+
 ### IMPORT DATABASE MODELS ###
 Base = declarative_base()
-from project.models import model_employees
+from gui.models import model_employees
 
 ### CREATE DATABASE TABLES BASED ON MODELS ###
 Base.metadata.create_all(db_engine) 
 
 ### DEFINE THE FLASK APP ###
-app = Flask('project', static_url_path='')
+app = Flask('gui', static_url_path='')
 app.debug = False
 
 ### ALLOW RELOAD OF TEMPLATE FILES DURING DEVELOPMENT ###
@@ -45,5 +46,8 @@ def index_redirect():
     return redirect(url_for('employees_index'))
 
 ### IMPORT CONTROLLERS ###
-from project.controllers import controller_employees
+from gui.controllers import controller_employees
 
+### START THE APP ###    
+if __name__ == '__main__':    
+    app.run(host="0.0.0.0",port="8080")
